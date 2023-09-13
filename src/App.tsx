cd7ba51165cc4ad4c8f8553b7cbee359f48703e9
lsj0202 <w1004123@gmail.com>
2023-09-13 23:17:11 +0900

    initial code

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<h1>Hello</h1>} />
+        <Route path="/posts" element={<h1>posts</h1>} />
+        <Route path="/posts/:id" element={<h1>posts:id</h1>} />
+        <Route path="/posts/new" element={<h1>posts:new</h1>} />
+        <Route path="/posts/edit/:id" element={<h1>posts:edit:id</h1>} />
+        <Route path="/profile" element={<h1>profile</h1>} />
+        <Route path="*" element={<Navigate replace to="/" />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
