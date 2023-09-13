ccb0f70bf519289c11702c7507808edf7d68afa2
lsj0202 <w1004123@gmail.com>
2023-09-14 00:19:19 +0900

    feat: add layout

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,14 +1,20 @@
+import Profile from 'pages/Profile';
+import Home from 'pages/home';
+import Posts from 'pages/posts';
+import Detail from 'pages/posts/detail';
+import Edit from 'pages/posts/edit';
+import New from 'pages/posts/new';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 const Router = () => {
   return (
     <Routes>
-      <Route path='/' element={<h1>Hello</h1>} />
-      <Route path='/posts' element={<h1>posts</h1>} />
-      <Route path='/posts/:id' element={<h1>posts:id</h1>} />
-      <Route path='/posts/new' element={<h1>posts:new</h1>} />
-      <Route path='/posts/edit/:id' element={<h1>posts:edit:id</h1>} />
-      <Route path='/profile' element={<h1>profile</h1>} />
+      <Route path='/' element={<Home />} />
+      <Route path='/posts' element={<Posts />} />
+      <Route path='/posts/:id' element={<Detail />} />
+      <Route path='/posts/new' element={<New />} />
+      <Route path='/posts/edit/:id' element={<Edit />} />
+      <Route path='/profile' element={<Profile />} />
       <Route path='*' element={<Navigate replace to='/' />} />
     </Routes>
   );
